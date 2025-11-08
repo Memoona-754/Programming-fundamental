f54d1b43f6d45bfd126cb1c3b77b29add71b1bf7
Memoona-754 <memoonamehboob754@gmail.com>
2025-11-08 20:37:44 +0500

    C language

--- LAB MANUAL EXERCIES/LAB 06/Q6.c
+++ LAB MANUAL EXERCIES/LAB 06/Q6.c
@@ -1,36 +1,10 @@
-/*#include<stdio.h>
-int main(){
-    int i,j,k,rows=5;
-      for(i=1;i<=rows;i++){
-          for(j=1;j<=rows;j++){
-            printf(" ");
-          }
-          for(k=1;k<=2*i-1;k++){
-            printf("*");
-          }
-          printf("\n");
-      }
-      for(i=rows-1;i<=1;i--){
-          for(j=1;j<=rows;j++){
-            printf(" ");
-          }
-          for(k=1;k<=2*i-1;k++){
-            printf("*");
-          }
-          printf("\n");
-      }
-      return 0;  
-}*/
-
 #include <stdio.h>
-
 int main() {
     int n = 5; // Number of rows for the top half, including the middle row
     int i, j, space;
 
     // Upper half of the diamond
     for (i = 1; i <= n; i++) {
-        // Print leading spaces
         for (space = 1; space <= n - i; space++) {
             printf(" ");
         }
@@ -42,8 +16,7 @@ int main() {
     }
     // Lower half of the diamond
     for (i = n - 1; i >= 1; i--) {
-        // Print leading spaces
-        for (space = 1; space <= n - i; space++) {
+        for (space = 1; space <= n - i; space++) {//printing space
             printf(" ");
         }
         // Print asterisks
