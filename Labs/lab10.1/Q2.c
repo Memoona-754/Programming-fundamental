2c5d940cf569891df741618c2007eb34c32a2550
Memoona-754 <memoonamehboob754@gmail.com>
2025-11-13 12:58:52 +0500

    C language

---
+++ Labs/lab10.1/Q2.c
@@ -0,0 +1,18 @@
+/*#include <stdio.h>
+void onlytype(int);
+int main()
+{
+    int a = 5;
+    printf("Calling function only type:\n");
+    onlytype(a);
+    return 0;
+}
+void onlytype(int a)
+{
+    printf("a is %d", a);
+}*/
+
+#include<stdio.h>
+int main(){
+    
+}
